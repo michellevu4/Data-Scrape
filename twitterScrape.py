4d5cbbc8f8bed09cd5d318ebefd8186537718282
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2022-10-16 23:51:28 -0400

    Print table of latest 10 tweets

--- twitterScrape.py
+++ twitterScrape.py
@@ -1,8 +1,22 @@
-import snscrape.modules.twitter as twitterScraper
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
 
-scraper = twitterScraper.TwitterHashtagScraper("MediaLitWeek")
+# scraper = twitterScraper.TwitterHashtagScraper("MediaLitWeek")
 
-for i, tweet in enumerate(scraper.get_items()):
-    if i > 2:
+# for i, tweet in enumerate(scraper.get_items()):
+#     if i > 2:
+#         break
+#     print(f"Tweet #{i} from {tweet.user}: {tweet.content}")
+
+# Created a list to append all tweet attributes(data)
+attributes_container = []
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i,tweet in enumerate(sntwitter.TwitterHashtagScraper('#MediaLitWeek').get_items()):
+    if i>10:
         break
-    print(f"Tweet #{i} from {tweet.user}: {tweet.content}")
+    attributes_container.append([tweet.date.date(), tweet.likeCount, tweet.user.username, tweet.content])
+    
+# Creating a dataframe from the tweets list above 
+tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Like Count", "User", "Tweets"])
+print(tweets_df)
