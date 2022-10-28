641a4ad3b5b7c397bf11c1755c441566111d117f
michellevu4 <michelle.vu@ufl.edu>
2022-10-28 00:03:38 -0400

    CSV

--- twitterScrape.py
+++ twitterScrape.py
@@ -9,10 +9,10 @@ print("#MediaLitWeek")
 for i,tweet in enumerate(sntwitter.TwitterHashtagScraper('#MediaLitWeek').get_items()):
     if i>10:
         break
-    attributes_container.append([tweet.date.date(), tweet.place, tweet.coordinates.longitude, tweet.Coordinates.latitude, tweet.likeCount, tweet.retweetCount, tweet.inReplyToUser, tweet.user.username, tweet.user.displayname, tweet.user.rawDescription, tweet.user.followersCount, tweet.user.verified, tweet.content])
+    attributes_container.append([tweet.date.date(), tweet.place, tweet.likeCount, tweet.retweetCount, tweet.inReplyToUser, tweet.user.username, tweet.user.displayname, tweet.user.rawDescription, tweet.user.followersCount, tweet.user.verified, tweet.content])
 
 # Creating a dataframe from the tweets list above
-tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Location", "Longitude", "Latitude", "Like Count", "Retweet Count", "In Reply To", "User", "User Name", "User Description", "Follower Count", "Verified", "Tweets"])
+tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Location", "Like Count", "Retweet Count", "In Reply To", "User", "User Name", "User Description", "Follower Count", "Verified", "Tweets"])
 print(tweets_df)
 
 # Created a list to append all tweet attributes(data)
@@ -23,10 +23,10 @@ print("#DigCitWeek")
 for i,tweet in enumerate(sntwitter.TwitterHashtagScraper('#DigCitWeek').get_items()):
     if i>10:
         break
-    attributes_container2.append([tweet.date.date(), tweet.place, tweet.coordinates.longitude, tweet.Coordinates.latitude, tweet.likeCount, tweet.retweetCount, tweet.inReplyToUser, tweet.user.username, tweet.user.displayname, tweet.user.rawDescription, tweet.user.followersCount, tweet.user.verified, tweet.content])
+    attributes_container2.append([tweet.date.date(), tweet.place, tweet.likeCount, tweet.retweetCount, tweet.inReplyToUser, tweet.user.username, tweet.user.displayname, tweet.user.rawDescription, tweet.user.followersCount, tweet.user.verified, tweet.content])
 
 # Creating a dataframe from the tweets list above
-tweets_df = pd.DataFrame(attributes_container2, columns=["Date Created", "Location", "Longitude", "Latitude", "Like Count", "Retweet Count", "In Reply To", "User", "User Name", "User Description", "Follower Count", "Verified", "Tweets"])
+tweets_df = pd.DataFrame(attributes_container2, columns=["Date Created", "Location", "Like Count", "Retweet Count", "In Reply To", "User", "User Name", "User Description", "Follower Count", "Verified", "Tweets"])
 print(tweets_df)
 
 # export the dataframe to a csv file if desired
